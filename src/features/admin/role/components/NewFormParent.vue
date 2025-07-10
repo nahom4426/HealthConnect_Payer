d61843052af47da9ddc48f86c425d59eb4494a96
HabenTesfay <haben.t@medcoanalytics.com>
2025-07-10 14:28:34 +0300

    Role form refactored

--- src/features/admin/role/components/NewFormParent.vue
+++ src/features/admin/role/components/NewFormParent.vue
@@ -20,14 +20,20 @@ const props = defineProps({
   onGoBack: {
     type: Function,
   },
+  isModal:{
+    type:Boolean,
+    default:true
+  },
   error: {
     type: String,
   },
 });
 </script>
 <template>
-  <div :class="[$style[size]]" class="overflow-hidden flex flex-col justify-between bg-white rounded-md">
-    <div class="flex justify-between border-b border-text-secondary-clr/30 items-center">
+  <div :class="[$style[size]]" class="overflow-hidden flex flex-col justify-between  bg-white rounded-md">
+   
+    <div class="flex flex-col gap-4 form-scrollbar flex-1 overflow-auto p-2 border-b border-text-secondary-clr/30">
+       <div class="flex justify-between border-b border-text-secondary-clr/30 items-center">
       <div class="flex items-center gap-4">
         <button @click="onGoBack" v-if="goBack"
           class="grid place-items-center rounded-md border border-text-clr"></button>
@@ -35,14 +41,13 @@ const props = defineProps({
       </div>
       <div class="flex items-center gap-4">
         <slot name="right-actions">
-          <button @click="closeModal()">X</button>
+          <button v-if="isModal" @click="closeModal()">X</button>
         </slot>
         <button class="border rounded-full">
           <!-- <BaseIcon :path="mdiClose" :size="20" /> -->
         </button>
       </div>
     </div>
-    <div class="flex flex-col gap-4 form-scrollbar flex-1 overflow-auto p-2 border-b border-text-secondary-clr/30">
       <slot />
       <!-- / <ResponseError :error="error" /> -->
     </div>
